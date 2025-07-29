12bae0ed4189b2c334afaf23cd26c42f8f764e7b
dalbsyn <rmrfmanwoman@gmail.com>
2025-07-29 12:56:08 +0500

    TEST PODPISANIE 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 answer = input("I ACCEPT ONLY YES OR NO:\n")
 
 if answer != "YES" and answer !="NO":
-    raise ValueError("I ACCEPT ONLY YES OR NOa")
+    raise ValueError("I ACCEPT ONLY YES OR NO")
 else:
     print("ISPOLNYAMYJ FAIL")
