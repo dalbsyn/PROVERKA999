a7ba7529e5ab804e4875d4f8bae19d019b65c2f5
dalbsyn <rmrfmanwoman@gmail.com>
2025-07-29 13:15:32 +0500

    DELETE NENUZHNO

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ answer = input("I ACCEPT ONLY YES OR NO:\n")
 if answer != "YES" and answer !="NO":
     raise ValueError("I ACCEPT ONLY YES OR NO")
 else:
-    print("VSCODE UMEET PODPISYVAT COMMITY?")
     print("ISPOLNYAMYJ FAIL.")
