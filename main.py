7f15de1bf2c9e4b009be867dd5b14fc097c4a36f
dalbsyn <rmrfmanwoman@gmail.com>
2025-07-29 12:52:36 +0500

    TEST SSH PODPISANIE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-answer = input("I ACCEPT ONLY YES OR NO")
+answer = input("I ACCEPT ONLY YES OR NO:\n")
 
 if answer != "YES" and answer !="NO":
-    raise ValueError
+    raise ValueError("I ACCEPT ONLY YES OR NOa")
 else:
     print("ISPOLNYAMYJ FAIL")
